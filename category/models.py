3c51d144e017910bc9b835278e07d0656dbdb5f3
Ali Sina Sultani <alisinasultani255@gmail.com>
2024-07-29 18:01:43 +0430

    category model is created

--- category/models.py
+++ category/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+class Category(models.Model):
+    CATEGORY_CHOICES = (
+        ("P", "Pending for Approval"),
+        ("R", "Rejected"),
+        ("A", "Approved"),
+        ("B", "Blocked"),
+    )
+    name = models.CharField("Category Name", max_length=24, unique=True)
+    description = models.TextField(
+        "Category Description", max_length=500, null=True, blank=True
+    )
+    created_on = models.DateTimeField(auto_now_add=True, verbose_name="Created On")
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name="Created By"
+    )
+    updated_on = models.DateTimeField("Last Updated On", auto_now=True)
+    status = models.CharField(
+        "Category Status", max_length=1, choices=CATEGORY_CHOICES, default="P"
+    )
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "Category"
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
